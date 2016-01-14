971515d2031e6cde7fbd7e4314deb96c03057e9b
Will O <napalmhammer@gmail.com>
2016-01-13 23:15:45 -0500

    Updating current

--- Phoenix/Phoenix-core/main.cpp
+++ Phoenix/Phoenix-core/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+#include "src\graphics\window.h"
+
 
 int main()
 {
-	std::cout << "help";
+	using namespace Phoenix;
+	using namespace graphics;
 
-	std::cout << " me...";
+	Window window("Phoenix Rebirth", 960, 540);
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	while (!window.closed())
+	{
+
+		std::cout << window.getWidth()
+			<< ", "
+			<< window.getHeight()
+			<< std::endl;
+		window.clear();
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+		window.update();
+	}
+
+	std::cout << "balls" << std::endl;
+	system("PAUSE");
 	return 0;
 }
